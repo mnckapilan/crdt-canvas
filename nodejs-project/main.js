192e3a5d10152712458d77a34a671b702847661a
kapilan <mnckapilan@gmail.com>
2019-10-10 19:37:01 +0100

    Remove left-pad dependency in nodejs

--- nodejs-project/main.js
+++ nodejs-project/main.js
@@ -1,6 +1,6 @@
 var http = require('http');
-var leftPad = require('left-pad');
-var versions_server = http.createServer( (request, response) => {
-  response.end('Versions: ' + JSON.stringify(process.versions) + ' left-pad: ' + leftPad(42, 5, '0'));
+
+var versions_server = http.createServer((request, response) => {
+  response.end('Hello there! The Node JS server is running');
 });
 versions_server.listen(3000);
