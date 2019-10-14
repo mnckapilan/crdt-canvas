3340cd6b1a76f39c3d1a84aa76ee3cdfe4a887b2
jackmorrison <jsm1317@ic.ac.uk>
2019-10-14 12:54:44 +0100

    Integrates whiteboard with existing backend

---
+++ Canvas/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  
+//
+//  Created by Jack Morrison on 14/10/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clearTapped() {
+        
+    }
+}
+
