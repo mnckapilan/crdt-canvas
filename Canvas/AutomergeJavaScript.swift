bbd627b067caeb6013971e20a4bf3c4c0c08eefe
serenechongtrakul <serenelc@outlook.com>
2019-10-16 14:25:55 +0100

    [sc5217, km2717] now calls javascript automerge function when strokes are drawn

--- Canvas/AutomergeJavaScript.swift
+++ Canvas/AutomergeJavaScript.swift
@@ -11,29 +11,28 @@ import JavaScriptCore
 class AutomergeJavaScript: NSObject {
     
     /// Singleton instance. Much more resource-friendly than creating multiple new instances.
-       static let shared = AutomergeJavaScript()
-       private let vm = JSVirtualMachine()
-       private let context: JSContext
+    static let shared = AutomergeJavaScript()
+    private let vm = JSVirtualMachine()
+    private let context: JSContext
+   
+    private override init() {
+        let jsCode = try? String.init(contentsOf: Bundle.main.url(forResource: "Canvas.bundle", withExtension: "js")!)
+        
+        // Create a new JavaScript context that will contain the state of our evaluated JS code.
+        self.context = JSContext(virtualMachine: self.vm)
        
-       private override init() {
-           let jsCode = try? String.init(contentsOf: Bundle.main.url(forResource: "Canvas.bundle", withExtension: "js")!)
-
-           
-           // Create a new JavaScript context that will contain the state of our evaluated JS code.
-           self.context = JSContext(virtualMachine: self.vm)
-           
-           // Evaluate the JS code that defines the functions to be used later on.
-           self.context.evaluateScript(jsCode)
-       }
+        // Evaluate the JS code that defines the functions to be used later on.
+        self.context.evaluateScript(jsCode)
+    }
        
     func javascript_func(completion: @escaping (_ randomNumber: Int) -> Void) {
-           // Run this asynchronously in the background
-           DispatchQueue.global(qos: .userInitiated).async {
-               var randomNumber = 0
-               let jsModule = self.context.objectForKeyedSubscript("Canvas")
-               let jsSynchronizer = jsModule?.objectForKeyedSubscript("Synchronizer")
-               
-               // In the JSContext global values can be accessed through `objectForKeyedSubscript`.
+        // Run this asynchronously in the background
+        DispatchQueue.global(qos: .userInitiated).async {
+            var randomNumber = 0
+            let jsModule = self.context.objectForKeyedSubscript("Canvas")
+            let jsSynchronizer = jsModule?.objectForKeyedSubscript("Synchronizer")
+           
+            // In the JSContext global values can be accessed through `objectForKeyedSubscript`.
             if let result = jsSynchronizer?.objectForKeyedSubscript("randomNumber").call(withArguments: []) {
                 randomNumber = Int(result.toInt32())
                }
@@ -42,6 +41,6 @@ class AutomergeJavaScript: NSObject {
                DispatchQueue.main.async {
                    completion(randomNumber)
                }
-           }
        }
+    }
 }
