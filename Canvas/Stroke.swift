2531421c8702d2eeb5eace56e03bb628a3d6eb80
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2019-11-20 15:06:28 +0000

    Adds support for partial deletion with lines and rectangles (#59)

--- Canvas/Stroke.swift
+++ Canvas/Stroke.swift
@@ -43,6 +43,8 @@ enum Change: Encodable {
         case point
         case identifier
         case index
+        case lower
+        case upper
     }
     
     func encode(to encoder: Encoder) throws {
@@ -66,6 +68,11 @@ enum Change: Encodable {
             try container.encode("PARTIAL_REMOVE_STROKE", forKey: CodingKeys.type)
             try container.encode(str, forKey: CodingKeys.identifier)
             try container.encode(i, forKey: CodingKeys.index)
+        case let .betterPartial(str, i, j):
+            try container.encode("BETTER_PARTIAL", forKey: CodingKeys.type)
+            try container.encode(str, forKey: CodingKeys.identifier)
+            try container.encode(i, forKey: CodingKeys.lower)
+            try container.encode(j, forKey: CodingKeys.upper)
         }
     }
     
@@ -74,6 +81,7 @@ enum Change: Encodable {
     case addPoint([Point], String)
     case clearCanvas
     case partialRemoveStroke(String, Int)
+    case betterPartial(String, Double, Double)
 }
 
 enum Mode {
@@ -84,8 +92,8 @@ enum Mode {
 }
 
 class Segment: Codable {
-    var start: Int
-    var end: Int
+    var start: Double
+    var end: Double
     
     enum CodingKeys: String, CodingKey {
         case start
@@ -94,11 +102,11 @@ class Segment: Codable {
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        start = try container.decode(Int.self, forKey: CodingKeys.start)
-        end = try container.decode(Int.self, forKey: CodingKeys.end)
+        start = try container.decode(Double.self, forKey: CodingKeys.start)
+        end = try container.decode(Double.self, forKey: CodingKeys.end)
     }
     
-    init(_ start: Int, _ end: Int) {
+    init(_ start: Double, _ end: Double) {
         self.start = start
         self.end = end
     }
@@ -154,7 +162,7 @@ class Stroke: Codable {
     init(points: [Point], colour: UIColor, isShape: Bool = false) {
         self.points = points
         self.colour = colour
-        self.segments = [Segment(0, self.points.count - 1)]
+        self.segments = [Segment(0.0, Double(self.points.count - 1))]
         self.isShape = isShape
     }
     
@@ -183,16 +191,102 @@ class Stroke: Codable {
         return indexOf(givenPoint: givenPoint) != nil
     }
     
-    func indexOf(givenPoint: Point) -> Int? {
-        for segment in segments {
-            for i in segment.start...segment.end {
-                let point = points[i]
-                if ((givenPoint.x <= point.x + 10 && givenPoint.x >= point.x - 10) && (givenPoint.y <= point.y + 10 && givenPoint.y >= point.y - 10)) {
-                    return i;
+    let ERROR_BOUND = Float(20)
+    
+    func getClosestPoint(_ a: Point, _ b: Point, _ p: Point) -> Double? {
+        let a_to_p = (
+            p.x - a.x,
+            p.y - a.y
+        )
+        let a_to_b = (
+            b.x - a.x,
+            b.y - a.y
+        )
+        let atb_2 = pow(a_to_b.0, 2) + pow(a_to_b.1, 2)
+        let atb_dot_atp = a_to_p.0 * a_to_b.0 + a_to_p.1 * a_to_b.1
+        let t = atb_dot_atp / atb_2
+        
+        let nPoint = (a.x + a_to_b.0 * t, a.y + a_to_b.1 * t)
+        
+        if sqrt(pow(p.x - nPoint.0, 2) + pow(p.y - nPoint.1, 2)) > ERROR_BOUND {
+            return nil
+        }
+        
+        return Double(t)
+    }
+    
+    func isInLine(_ lPoint: Point, _ uPoint: Point, _ givenPoint: Point) -> Double? {
+        if lPoint.x == uPoint.x {
+            if abs(givenPoint.x - lPoint.x) > ERROR_BOUND {
+                return nil
+            }
+            let ratio = (givenPoint.y - lPoint.y) / (uPoint.y - lPoint.y)
+            //print("ratio 2: ", ratio)
+            if ratio < 0 || ratio > 1 {
+                return nil
+            }
+            return Double(ratio)
+        } else {
+            let ratio = (givenPoint.x - lPoint.x) / (uPoint.x - lPoint.x)
+            print("ratio 1: ", ratio)
+            if ratio < 0 || ratio > 1 {
+                return nil
+            }
+            let interpolatedY = (uPoint.y - lPoint.y) * ratio + lPoint.y
+            print("ys: ", interpolatedY, givenPoint.y)
+            if abs(interpolatedY - givenPoint.y) > ERROR_BOUND {
+                return nil
+            }
+            return Double(ratio)
+        }
+    }
+    
+    func indexOf(givenPoint: Point) -> Double? {
+        if isShape {
+            for segment in segments {
+                var i = segment.start // 1, 1.5
+                while i < segment.end {
+                    let l = Int(floor(i)) // 1, 1
+                    let u = Int(floor(i + 1)) // 2, 2
+                    
+                    let lPoint = points[l]
+                    let uPoint = points[u]
+                    
+                    let t = getClosestPoint(lPoint, uPoint, givenPoint)
+                    if let tNotNil = t {
+                        let k = Double(l) + tNotNil
+                        if k > i && k < segment.end {
+                            return k
+                        }
+                    }
+                    
+                    
+                    i += 1
+                    i = floor(i)
+                }
+            }
+        } else {
+            for segment in segments {
+                for i in Int(ceil(segment.start))...Int(floor(segment.end)) {
+                    let point = points[i]
+                    if ((givenPoint.x <= point.x + 10 && givenPoint.x >= point.x - 10) && (givenPoint.y <= point.y + 10 && givenPoint.y >= point.y - 10)) {
+                        return Double(i)
+                    }
                 }
             }
         }
         return nil;
+    }
+    
+    func getPoint(_ i: Double) -> Point {
+        let lower = Int(floor(i))
+        let upper = Int(ceil(i))
+        let lPoint = points[lower]
+        let uPoint = points[upper]
+        let uWeight = Float(i - floor(i))
+        let lWeight = 1 - uWeight
+        let p = Point(x: lWeight * lPoint.x + uWeight * uPoint.x, y: lWeight * lPoint.y + uWeight * uPoint.y)
+        return p
     }
     
     var cgPath: CGPath {
@@ -201,11 +295,21 @@ class Stroke: Codable {
                 let path = UIBezierPath.init()
                 path.lineCapStyle = CGLineCap.round
                 path.lineWidth = 3
-                path.move(to: points[0].cgPoint)
-                for i in 1...points.count - 1 {
-                    path.addLine(to: points[i].cgPoint)
+
+                for segment in segments {
+                    path.move(to: getPoint(segment.start).cgPoint)
+                    
+                    var i = segment.start
+                    while i < segment.end {
+                        i += 1
+                        i = floor(i)
+                        i = min(i, segment.end)
+                        
+                        path.addLine(to: getPoint(i).cgPoint)
+                    }
+                    
+                    print("draw: ", segment.start, segment.end)
                 }
-                path.addLine(to: points[0].cgPoint)
                 
                 return path.cgPath
 
@@ -223,7 +327,7 @@ class Stroke: Codable {
                         continue
                     }
                     
-                    for i in segment.start...segment.end {
+                    for i in Int(ceil(segment.start))...Int(floor(segment.end)) {
                         let point = points[i]
                         
                         if s == 0 {
